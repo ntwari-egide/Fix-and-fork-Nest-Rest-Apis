2f28da13305cacda8a09babc58d1b2f86ff20e11
ntwari egide <ntwariegide2@gmail.com>
2021-10-30 00:44:51 -0700

    ft(user-modal): impelement user modal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://root:edaedaeda@cluster0.ulkpq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
+  imports: [ UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,24 @@
+/**
+ * @author: ntwari egide
+ * @description: User modal implementation
+ */
+
+import * as mongoose from 'mongoose';
+
+export const UserSchema = new mongoose.Schema({
+  id: Number,
+
+  role: Number,
+
+  email: String,
+
+  username: String,
+
+  password: String,
+
+  joinedAt: Date,
+
+  updatedAt: Date
+
+
+});
