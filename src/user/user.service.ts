67007fbbb070dccdec85d4764e373c42564bc74e
ntwari egide <ntwariegide2@gmail.com>
2021-10-30 11:08:50 -0700

    ft(user-service): add user service implementation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,6 +9,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './user.interface';
 import * as bcrypt from 'bcrypt';
+import { UserNotFoundException } from 'src/exceptions/UserNotFoundException';
 
 @Injectable()
 export class UserService {
@@ -35,15 +36,34 @@ export class UserService {
     return this.userModal.find().exec()
   }
 
-  async findOne(id: number): Promise<User> {
-    return null
+  checkUserExistance = (id: String) : User => {
+    let user : any
+    try {
+      user = this.userModal.findById(id).exec()
+    } catch (error) {
+      throw new UserNotFoundException('User with id '+id+ ' is not found')
+    }
+
+    return user
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOne(id: String): Promise<User> {
+    return this.checkUserExistance(id)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: String, updateUserDto: UpdateUserDto): Promise<User> {
+    
+    this.checkUserExistance(id)
+
+    return this.userModal.findByIdAndUpdate(id,updateUserDto).exec()
+
+  }
+
+  async remove(id: String) {
+    
+    this.checkUserExistance(id)
+
+    return this.userModal.findByIdAndRemove(id).exec()
+
   }
 }
