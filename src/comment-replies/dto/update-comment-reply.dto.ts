03cdd4e42691053a7d39294586334a09a81e1684
ntwari egide <ntwariegide2@gmail.com>
2021-10-31 14:00:07 -0700

    ft(comment replies): implement comment replies

--- src/comment-replies/dto/update-comment-reply.dto.ts
+++ src/comment-replies/dto/update-comment-reply.dto.ts
@@ -1,4 +1,31 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateCommentReplyDto } from './create-comment-reply.dto';
 
-export class UpdateCommentReplyDto extends PartialType(CreateCommentReplyDto) {}
+export class UpdateCommentReplyDto extends PartialType(CreateCommentReplyDto) {
+
+    @ApiProperty({
+        description: "Comment Ids",
+        type: String
+    })
+    commentId: String
+
+    @ApiProperty({
+        description: "Comment reply message content",
+        type: String
+    })
+    replyMessage: String
+
+    @ApiProperty({
+        description: "Replied at",
+        type: Date
+    })
+    repliedAt: Date
+
+
+    @ApiProperty({
+        description: "Replied by",
+        type: String
+    })
+    repliedBy: String
+
+}
